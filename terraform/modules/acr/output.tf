991c1890b7de8e69a04a2dad6519d1c095e323d6
vinaypalvirk51 <115966015+vinaypalvirk51@users.noreply.github.com>
2025-08-16 21:26:53 -0400

    Update output.tf

--- terraform/modules/acr/output.tf
+++ terraform/modules/acr/output.tf
@@ -1,7 +1,7 @@
 output "acr_id" {
-  value = azurerm_container_registry.my_acr
+  value = azurerm_container_registry.my_acr.id
 }
 
 output "acr_login_server" {
-  value = azurerm_container_registry.my_acr
+  value = azurerm_container_registry.my_acr.login_server
 }
