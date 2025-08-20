c64851d8a839066ea7e78c5129485824a782b4f4
sukhdeep26 <116036793+sukhdeep26@users.noreply.github.com>
2025-08-19 21:57:42 -0400

    Update main.tf

--- terraform/modules/rbac/main.tf
+++ terraform/modules/rbac/main.tf
@@ -1,5 +1,12 @@
-resource "azurerm_role_assignment" "kv_access" {
-  scope                = var.key_vault_id
-  role_definition_name = var.role_name
-  principal_id         = var.principal_object_id
+#resource "azurerm_role_assignment" "kv_access" {
+#  scope                = var.key_vault_id
+#  role_definition_name = var.role_name
+#  principal_id         = var.principal_object_id
+#}
+
+resource "azurerm_role_assignment" "this" {
+  for_each             = { for idx, a in var.assignments : idx => a }
+  principal_id         = each.value.principal_id
+  role_definition_name = each.value.role_definition
+  scope                = each.value.scope
 }
