5d9785522c973a3ea692bffbd6d3766ff4b7dc46
sukhdeep26 <116036793+sukhdeep26@users.noreply.github.com>
2025-08-19 22:15:00 -0400

    Update main.tf

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -13,6 +13,14 @@ resource "azurerm_kubernetes_cluster" "my_aks" {
   identity {
     type = "SystemAssigned"
   }
+network_profile {
+    network_plugin     = "azure"
+    network_policy     = "azure"
+    dns_service_ip     = "10.2.0.10"
+    docker_bridge_cidr = "172.17.0.1/16"
+    service_cidr       = "10.2.0.0/24"
+    subnet_id          = var.vnet_subnet_id
+  }
 
   tags = {
     Environment = var.environment
