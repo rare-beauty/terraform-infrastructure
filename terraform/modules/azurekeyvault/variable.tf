efce0d83f90576f09fc6a4a00cc4538d0fbf305a
sukhdeep26 <116036793+sukhdeep26@users.noreply.github.com>
2025-08-31 19:33:49 -0400

    Update variable.tf

--- terraform/modules/azurekeyvault/variable.tf
+++ terraform/modules/azurekeyvault/variable.tf
@@ -1,42 +1,71 @@
-variable "kv_name" {
-  description = "Name of the Azure Key Vault"
-  type        = string
-}
-
-variable "location" {
-  description = "Location of the Key Vault"
-  type        = string
-}
-
-variable "rg_name" {
-  description = "Name of the Resource Group"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
+variable "kv_name"  { type = string }
+variable "location" { type = string }
+variable "rg_name"  { type = string }
+variable "tenant_id" { type = string }
 
 variable "sku_name" {
-  description = "SKU for the Key Vault"
-  type        = string
-  default     = "standard"
+  type    = string
+  default = "standard"
 }
 
 variable "purge_protection_enabled" {
-  description = "Enable purge protection"
-  type        = bool
-  default     = true
+  type    = bool
+  default = true
 }
 
 variable "soft_delete_retention_days" {
-  description = "Soft delete retention in days"
-  type        = number
-  default     = 7
+  type    = number
+  default = 7
 }
 
 variable "enable_rbac_authorization" {
   type    = bool
   default = true
+}
+
+# 🔒 Networking hardening
+variable "public_network_access_enabled" {
+  type    = bool
+  default = false   # disable public access by default
+}
+
+variable "kv_ip_rules" {
+  description = "List of public IPs to allow (optional)"
+  type        = list(string)
+  default     = []
+}
+
+variable "kv_vnet_subnet_ids" {
+  description = "List of subnet IDs to allow (optional)"
+  type        = list(string)
+  default     = []
+}
+
+# 🔒 Private Endpoint options
+variable "enable_private_endpoint" {
+  type    = bool
+  default = true
+}
+
+variable "subnet_id" {
+  description = "Subnet for the Private Endpoint NIC (required if PE enabled)"
+  type        = string
+  default     = null
+}
+
+variable "create_dns_zone" {
+  description = "Create private DNS zone for Key Vault"
+  type        = bool
+  default     = true
+}
+
+variable "vnet_id" {
+  description = "VNet to link DNS zone to (needed if create_dns_zone = true)"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
 }
