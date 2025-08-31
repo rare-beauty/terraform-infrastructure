16e250dbbaba3487da786d6de8bda6c71f6839a3
sukhdeep26 <116036793+sukhdeep26@users.noreply.github.com>
2025-08-31 19:34:54 -0400

    Update output.tf

--- terraform/modules/azurekeyvault/output.tf
+++ terraform/modules/azurekeyvault/output.tf
@@ -1,14 +1,3 @@
-output "key_vault_id" {
-  description = "Key Vault ID"
-  value       = azurerm_key_vault.my_kv.id
-}
-
-output "key_vault_name" {
-  description = "Key Vault Name"
-  value       = azurerm_key_vault.my_kv.name
-}
-
-output "key_vault_uri" {
-  description = "Key Vault URI"
-  value       = azurerm_key_vault.my_kv.vault_uri
-}
+output "key_vault_id"   { value = azurerm_key_vault.my_kv.id }
+output "key_vault_name" { value = azurerm_key_vault.my_kv.name }
+output "key_vault_uri"  { value = azurerm_key_vault.my_kv.vault_uri }
