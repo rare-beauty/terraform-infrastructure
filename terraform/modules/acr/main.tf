212f1799d6083ffcc71bad79a825722445d3f286
sukhdeep26 <116036793+sukhdeep26@users.noreply.github.com>
2025-08-31 20:05:48 -0400

    Update main.tf

--- terraform/modules/acr/main.tf
+++ terraform/modules/acr/main.tf
@@ -1,3 +1,5 @@
+# checkov:skip=CKV_AZURE_233: Not using Premium/zone redundancy in dev
+# checkov:skip=CKV_AZURE_139: Public access allowed in dev; will move to PE later
 resource "azurerm_container_registry" "my_acr" {
   name                = var.acr_name
   resource_group_name = var.resource_group_name
