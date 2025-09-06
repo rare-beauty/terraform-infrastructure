8fa76f25b3ebad794e825487917f015a2689b2e6
sukhdeep26 <116036793+sukhdeep26@users.noreply.github.com>
2025-09-06 14:16:08 -0400

    Update main.tf

--- terraform/modules/rbac/main.tf
+++ terraform/modules/rbac/main.tf
@@ -4,9 +4,17 @@
 #  principal_id         = var.principal_object_id
 #}
 
+#resource "azurerm_role_assignment" "this" {
+ # for_each             = { for idx, a in var.assignments : idx => a }
+ # principal_id         = each.value.principal_id
+ # role_definition_name = each.value.role_definition
+ # scope                = each.value.scope
+#}
+
 resource "azurerm_role_assignment" "this" {
-  for_each             = { for idx, a in var.assignments : idx => a }
-  principal_id         = each.value.principal_id
-  role_definition_name = each.value.role_definition
-  scope                = each.value.scope
+  count                = var.enabled ? length(var.assignments) : 0
+  principal_id         = var.assignments[count.index].principal_id
+  role_definition_name = var.assignments[count.index].role_definition
+  scope                = var.assignments[count.index].scope
 }
+
