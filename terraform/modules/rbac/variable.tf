20c71dd40eaa627a90281634f38b9ddd25ae08fc
vinaypalvirk51 <115966015+vinaypalvirk51@users.noreply.github.com>
2025-09-14 17:48:08 -0400

    Update variable.tf

--- terraform/modules/rbac/variable.tf
+++ terraform/modules/rbac/variable.tf
@@ -1,26 +1,11 @@
-#variable "key_vault_id" {
- # description = "ID of the Key Vault"
- # type        = string
-#}
-
-#variable "role_name" {
-#  description = "RBAC role name to assign"
-#  type        = string
-#}
-
-#variable "principal_object_id" {
-#  description = "Object ID of the principal (user, service principal, managed identity)"
-#  type        = string
-#}
-
 variable "assignments" {
   description = "List of role assignments"
-  type = list(object({
+  type = map(object({
     principal_id    = string
     role_definition = string
     scope           = string
   }))
-  default = []
+  default = {}
 }
 
 variable "enabled" {
